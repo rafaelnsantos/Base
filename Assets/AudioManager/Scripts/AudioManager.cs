c56cb6a51414a95451153bc78bf1d3f4a68d5b74
Rafael Santos <rafaelnsantos1@gmail.com>
2017-12-05 20:55:32 -0200

    test commit github for unity
    teste

--- Assets/AudioManager/Scripts/AudioManager.cs
+++ Assets/AudioManager/Scripts/AudioManager.cs
@@ -1,104 +1,108 @@
 ﻿using UnityEngine;
 
-namespace AudioManager.scripts {
-    public class AudioManager : MonoBehaviour {
-        private AudioSource musicSource, effectSource;
+public class AudioManager : MonoBehaviour {
+    private AudioSource musicSource, effectSource;
 
-        private bool musicOn, effectOn;
+    private bool musicOn, effectOn;
 
-        public bool MusicOn {
-            get { return musicOn; }
+    public bool MusicOn {
+        get { return musicOn; }
+    }
+
+    public bool EffectOn {
+        get { return effectOn; }
+    }
+
+    public static AudioManager Instance;
+
+    void Awake () {
+        // Singleton Patter
+        if (Instance == null) {
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(gameObject);
         }
+        DontDestroyOnLoad(gameObject);
 
-        public bool EffectOn {
-            get { return effectOn; }
-        }
+        musicSource = gameObject.GetComponents<AudioSource>()[0];
+        effectSource = gameObject.GetComponents<AudioSource>()[1];
+    }
 
-        public static AudioManager Instance = null;
+    private void Start () {
+        musicOn = true;
+        effectOn = true;
+    }
 
-        void Awake () {
-            // Singleton Patter
-            if (Instance == null) {
-                Instance = this;
-            } else if (Instance != this) {
-                Destroy(gameObject);
-            }
-            DontDestroyOnLoad(gameObject);
+    /// <summary>
+    /// Play an effect.
+    /// </summary>
+    /// <param name="clip">Clip to play.</param>
+    /// <param name="duration">Duration, default 1.</param>
+    /// <param name="volume">Volume, default 1.</param>
+    /// <param name="pitch">Pitch, default 1.</param>
+    public void PlayEffect (AudioClip clip, float duration = 1f, float volume = 1f, float pitch = 1f) {
+        if (!effectOn) return;
 
-            musicSource = gameObject.GetComponents<AudioSource>()[0];
-            effectSource = gameObject.GetComponents<AudioSource>()[1];
-        }
+        effectSource.volume = volume;
+        effectSource.pitch = pitch;
+        effectSource.PlayOneShot(clip, duration);
+    }
 
-        private void Start () {
-            musicOn = true;
-            effectOn = true;
-        }
+    /// <summary>
+    /// Play a random effect.
+    /// </summary>
+    /// <param name="clips">Array of Clips to play.</param>
+    /// <param name="duration">Duration, default 1.</param>
+    /// <param name="volume">Volume, default 1.</param>
+    /// <param name="lowPitchRange">Low Pitch, defalt 0.95</param>
+    /// <param name="highPitchRange">High Pitch, defaltt 1.05</param>
+    public void PlayRandomEffect (AudioClip[] clips, float duration = 1f, float volume = 1f,
+        float lowPitchRange = 0.95f, float highPitchRange = 1.0f) {
+        int random = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
 
-        /// <summary>
-        /// Play an effect.
-        /// </summary>
-        /// <param name="clip">Clip to play.</param>
-        /// <param name="duration">Duration, default 1.</param>
-        /// <param name="volume">Volume, default 1.</param>
-        /// <param name="pitch">Pitch, default 1.</param>
-        public void PlayEffect (AudioClip clip, float duration = 1f, float volume = 1f, float pitch = 1f) {
-            if (!effectOn) return;
+        PlayEffect(clips[random], duration, volume, randomPitch);
+    }
 
-            effectSource.volume = volume;
-            effectSource.pitch = pitch;
-            effectSource.PlayOneShot(clip, duration);
-        }
+    /// <summary>
+    /// Play a music.
+    /// </summary>
+    /// <param name="clip">Music to play.</param>
+    public void PlayMusic (AudioClip clip) {
+        if (!musicOn) return;
 
-        /// <summary>
-        /// Play a random effect.
-        /// </summary>
-        /// <param name="clips">Array of Clips to play.</param>
-        /// <param name="duration">Duration, default 1.</param>
-        /// <param name="volume">Volume, default 1.</param>
-        /// <param name="lowPitchRange">Low Pitch, defalt 0.95</param>
-        /// <param name="highPitchRange">High Pitch, defaltt 1.05</param>
-        public void PlayRandomEffect (AudioClip[] clips, float duration = 1f, float volume = 1f,
-            float lowPitchRange = 0.95f, float highPitchRange = 1.0f) {
-            int random = UnityEngine.Random.Range(0, clips.Length);
-            float randomPitch = UnityEngine.Random.Range(lowPitchRange, highPitchRange);
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
 
-            PlayEffect(clips[random], duration, volume, randomPitch);
-        }
+    /// <summary>
+    /// Switch music on or off. Returns actual state.
+    /// </summary>
+    public bool SwitchMusic () {
+        musicOn = !musicOn;
 
-        /// <summary>
-        /// Play a music.
-        /// </summary>
-        /// <param name="clip">Music to play.</param>
-        public void PlayMusic (AudioClip clip) {
-            if (!musicOn) return;
-
-            musicSource.clip = clip;
+        if (!musicOn) {
+            musicSource.Stop();
+        } else {
             musicSource.Play();
         }
 
-        /// <summary>
-        /// Switch music on or off.
-        /// </summary>
-        /// <returns>Returns a boolean based on the status.</returns>
-        public bool SwitchMusic () {
-            musicOn = !musicOn;
+        return musicOn;
+    }
 
-            if (!musicOn) {
-                musicSource.Stop();
-            } else {
-                musicSource.Play();
-            }
+    /// <summary>
+    /// Switch effects on or off. Returns actual state.
+    /// </summary>
+    public bool SwitchEffect () {
+        effectOn = !effectOn;
+        return effectOn;
+    }
 
-            return musicOn;
-        }
+    public void SetEffectVolume (int volume) {
+        effectSource.volume = volume;
+    }
 
-        /// <summary>
-        /// Switch effects on or off.
-        /// </summary>
-        /// <returns>Returns a boolean based on the status.</returns>
-        public bool SwitchEffect () {
-            effectOn = !effectOn;
-            return effectOn;
-        }
+    public void SetMusivVolume (int volume) {
+        musicSource.volume = volume;
     }
 }
--- Assets/_Projeto/Scripts/Teste.cs
+++ Assets/_Projeto/Scripts/Teste.cs
@@ -6,11 +6,8 @@ using UnityEngine.UI;
 public class Teste : MonoBehaviour {
 	private Text asdd;
 
-	private Asd asd;
-
 	// Use this for initialization
 	void Start () {
-		asd = gameObject.AddComponent<Asd>();
 	}
 
 	// Update is called once per frame
