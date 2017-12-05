31de62392914ce37b8fac4ffaeb9de113a6c3b97
Rafael Santos <rafaelnsantos1@gmail.com>
2017-12-05 20:18:30 -0200

    first commit

---
+++ Assets/_Projeto/Scripts/Teste.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teste : MonoBehaviour {
+	private Text asdd;
+
+	private Asd asd;
+
+	// Use this for initialization
+	void Start () {
+		asd = gameObject.AddComponent<Asd>();
+	}
+
+	// Update is called once per frame
+	void Update () { }
+}
