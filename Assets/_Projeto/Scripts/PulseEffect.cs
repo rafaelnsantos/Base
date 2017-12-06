f817f1a4111bab440106c53a69b2ae3540beb7dd
Rafael Santos <rafaelnsantos1@gmail.com>
2017-12-06 04:58:14 -0200

    rotate and pulse effects

---
+++ Assets/_Projeto/Scripts/PulseEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class PulseEffect : MonoBehaviour {
+    // Grow parameters
+    [Range(0.1f, 5f)] public float approachSpeed = 0.02f;
+    [Range(1f, 5f)] public float growthBound = 2f;
+    [Range(0f, 1f)] public float shrinkBound = 0.5f;
+
+
+    private bool go = true;
+
+    private void Update () {
+        if (!go) return;
+
+        go = false;
+        
+        Shrink();
+    }
+
+    private void Grow () {
+        transform.DOScale(Vector3.one * growthBound, approachSpeed).OnComplete(()=> go = true);
+    }
+
+    private void Shrink () {
+        transform.DOScale(Vector3.one * shrinkBound, approachSpeed).OnComplete(Grow);
+    }
+}
---
+++ Assets/_Projeto/Scripts/RotateEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class RotateEffect : MonoBehaviour {
+    [Range(5f, 200f)] public float Speed;
+    public bool clockwise;
+
+    private void Update () {
+        transform.Rotate(clockwise ? Vector3.back : Vector3.forward, Speed * Time.deltaTime);
+    }
+}
