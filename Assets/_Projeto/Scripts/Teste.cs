da268af3a5b8f6566b965beff4cf5655b59a48a9
Rafael Santos <rafaelnsantos1@gmail.com>
2017-12-05 22:43:10 -0200

    teste

--- Assets/_Projeto/Scripts/Teste.cs
+++ Assets/_Projeto/Scripts/Teste.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class Teste : MonoBehaviour {
 
+	private AudioManager audioManager;
+	public AudioClip AudioClip;
+	
 	// Use this for initialization
 	void Start () {
-		AudioManager.Instance.SetEffectVolume(0.5f);
+		audioManager = AudioManager.Instance;
 	}
 
 	// Update is called once per frame
-	void Update () { }
+	void Update () {
+		audioManager.PlayMusic(AudioClip);
+	}
 }
