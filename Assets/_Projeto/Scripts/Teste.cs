d84585a6d3e8f9fbaa2deb7bfc9e160609873cf9
Rafael Santos <rafaelnsantos1@gmail.com>
2017-12-07 01:21:28 -0200

    teste

--- Assets/_Projeto/Scripts/Teste.cs
+++ Assets/_Projeto/Scripts/Teste.cs
@@ -1,17 +1,16 @@
 ﻿using UnityEngine;
 
 public class Teste : MonoBehaviour {
+    private AudioManager audioManager;
+    public AudioClip AudioClip;
 
-	private AudioManager audioManager;
-	public AudioClip AudioClip;
+// Use this for initialization
+    void Start () {
+        audioManager = AudioManager.Instance;
+    }
 
-	// Use this for initialization
-	void Start () {
-		audioManager = AudioManager.Instance;
-	}
-
-	// Update is called once per frame
-	void Update () {
-		audioManager.PlayMusic(AudioClip);
-	}
+// Update is called once per frame
+    void Update () {
+        audioManager.PlayMusic(AudioClip);
+    }
 }
