b1e5bb0e4ef107d560c0323a1054f1a9cdd28c3e
Rafael Santos <rafaelnsantos1@gmail.com>
2017-12-07 04:29:13 -0200

    sandbox folder

---
+++ Assets/_Projeto/Sandbox/ButtonClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour {
+
+	public AudioClip Audio;
+
+	public void PlayClick () {
+		AudioManager.Instance.PlayEffect(Audio);
+	}
+}
