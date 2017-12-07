7e619af5fb4cf3bd3e2aca0775d9d6980e831c99
Rafael Santos <rafaelnsantos1@gmail.com>
2017-12-07 04:28:59 -0200

    language manager

---
+++ Assets/LanguageManager/Scripts/LanguageSettings.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using SmartLocalization;
+
+public class LanguageSettings : MonoBehaviour {
+    private List<SmartCultureInfo> availableLanguages;
+    private LanguageManager languageManager;
+
+    public SmartCultureInfo ActualLanguage;
+
+    public static LanguageSettings Instance { get; private set; }
+
+    private void Awake () {
+        // Singleton Patter
+        if (Instance == null) {
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        languageManager = LanguageManager.Instance;
+    }
+
+    private void Start () {
+        SmartCultureInfo deviceCulture = languageManager.GetDeviceCultureIfSupported();
+
+        if (languageManager.NumberOfSupportedLanguages > 0) {
+            availableLanguages = languageManager.GetSupportedLanguages();
+        } else {
+            Debug.LogError(
+                "No languages are created!, Open the Smart Localization plugin at Window->Smart Localization and create your language!");
+        }
+
+        if (deviceCulture != null) {
+            SetLanguage(deviceCulture);
+        } else {
+            Debug.Log("The device language is not available in the current application. Loading default.");
+            SetLanguage(availableLanguages[1]);
+        }
+    }
+
+    private void SetLanguage (SmartCultureInfo language) {
+        ActualLanguage = language;
+        languageManager.ChangeLanguage(language);
+    }
+
+    public void SwitchLanguage () {
+        int actual = ActualLanguage == availableLanguages[0] ? 1 : 0;
+        SetLanguage(availableLanguages[actual]);
+    }
+}
---
+++ Assets/LanguageManager/Scripts/TextController.cs
@@ -0,0 +1,28 @@
+﻿using SmartLocalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+	private Text text;
+	public string Key;
+
+	private void Awake () {
+		text = GetComponent<Text>();
+	}
+	
+	private void Start () {
+		text.text = LanguageManager.Instance.GetTextValue(Key);
+		LanguageManager.Instance.OnChangeLanguage += OnLanguageChanged;
+	}
+	
+	private void OnLanguageChanged (LanguageManager languageManager) {
+		text.text = languageManager.GetTextValue(Key);
+	}
+	
+	private void OnDestroy () {
+		if (LanguageManager.HasInstance) {
+			LanguageManager.Instance.OnChangeLanguage -= OnLanguageChanged;
+		}
+	}
+}
