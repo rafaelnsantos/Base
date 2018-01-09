3672466222805094c348ede4a339dbd8f6985402
Rafael Santos <rafaelnsantos1@gmail.com>
2018-01-09 08:55:06 -0200

    add funcao para parar musica

--- Assets/2Dverse/AudioManager/Scripts/AudioSettings.cs
+++ Assets/2Dverse/AudioManager/Scripts/AudioSettings.cs
@@ -83,6 +83,10 @@ public class AudioSettings : Settings {
 		musicSource.Play();
 	}
 
+	public void StopMusic () {
+		musicSource.Stop();
+	}
+
 	/// <summary>
 	/// Switch music on or off. 
 	/// </summary>
