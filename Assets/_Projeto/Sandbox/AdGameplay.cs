002125b94cf22ecf5309a48dcb07cde2daf18c2c
Rafael Santos <rafaelnsantos1@gmail.com>
2018-01-09 08:56:39 -0200

    ad com reload por delay

--- Assets/_Projeto/Sandbox/AdGameplay.cs
+++ Assets/_Projeto/Sandbox/AdGameplay.cs
@@ -1,16 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using GoogleMobileAds.Api;
 using UnityEngine;
 
 public class AdGameplay : MonoBehaviour {
 
+	public float Delay;
+	public string AndroidBannerId, IphoneBannerId;
+	private BannerView banner;
+	private bool adLoading;
+	private string bannerId;
+
 	private void Start () {
-		AdMobiManager.Instance.RequestBanner(AdSize.SmartBanner, AdPosition.Top);
+#if UNITY_EDITOR
+		bannerId = "unused";
+#elif UNITY_ANDROID
+        bannerId = AndroidBannerId;
+#elif UNITY_IPHONE
+        bannerId = IphoneBannerId;
+#endif
+	}
+
+	private void Update () {
+		if (!adLoading) StartCoroutine(LoadAd());
 	}
 
 	private void OnDestroy () {
-		AdMobiManager.Instance.bannerView.Destroy();
+		banner.Destroy();
+	}
+
+	private void AdLoaded (object sender, EventArgs args) {
+		adLoading = false;
+		banner.OnAdLoaded -= AdLoaded;
+	}
+
+	private IEnumerator LoadAd () {
+		adLoading = true;
+		yield return new WaitForSeconds(Delay);
+
+		banner = AdMobiManager.Instance.RequestBanner(bannerId, AdSize.SmartBanner, AdPosition.Top);
+		banner.OnAdLoaded += AdLoaded;
 	}
 
 }
