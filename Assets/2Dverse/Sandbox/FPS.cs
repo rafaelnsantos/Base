2cfb94704dd64bb4e7af799849f8985c3787ba87
Rafael Santos <rafaelnsantos1@gmail.com>
2018-01-09 08:55:39 -0200

    fps no canto da tela

---
+++ Assets/2Dverse/Sandbox/FPS.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour {
+	
+	public float Frequency = 1.0f;
+	public Text fps;
+
+	private void Awake () {
+		DontDestroyOnLoad(this);
+	}
+
+	void Start () {
+		StartCoroutine(CountFps());
+	}
+
+	private IEnumerator CountFps () {
+		for (;;) {
+			// Capture frame-per-second
+			int lastFrameCount = Time.frameCount;
+			float lastTime = Time.realtimeSinceStartup;
+			yield return new WaitForSeconds(Frequency);
+
+			float timeSpan = Time.realtimeSinceStartup - lastTime;
+			int frameCount = Time.frameCount - lastFrameCount;
+
+			// Display it
+			int framesPerSec = Mathf.RoundToInt(frameCount / timeSpan);
+			fps.text = framesPerSec.ToString();
+		}
+	}
+
+}
