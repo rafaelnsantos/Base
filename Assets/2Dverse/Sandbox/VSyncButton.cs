a785dc6bbd05c97d8e725f5dedb91fa56e11c1d7
Rafael Santos <rafaelnsantos1@gmail.com>
2018-01-10 02:32:22 -0200

    botao para escolher entre 30 e 60 fps

---
+++ Assets/2Dverse/Sandbox/VSyncButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VSyncButton : Settings {
+
+	public Text text;
+
+	private void Start () {
+		Load();
+		GetComponent<Button>().onClick.AddListener(Switch);
+		SetText();
+	}
+
+	private void Switch () {
+		QualitySettings.vSyncCount = QualitySettings.vSyncCount == 1 ? 2 : 1;
+		SetText();
+	}
+
+	protected override void Save () {
+		SaveManager.SetInt("vsync", QualitySettings.vSyncCount);
+	}
+
+	protected override void Load () {
+		QualitySettings.vSyncCount = SaveManager.GetInt("vsync", 2);
+	}
+
+	private void SetText () {
+		int refreshRate = Screen.currentResolution.refreshRate;
+		refreshRate = QualitySettings.vSyncCount == 1 ? refreshRate: (refreshRate / 2);
+		text.text = refreshRate.ToString();
+	}
+
+}
