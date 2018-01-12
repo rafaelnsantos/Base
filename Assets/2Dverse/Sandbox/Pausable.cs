cba30e1bd59708fd7f901189b05f8e5755322be0
Rafael Santos <rafaelnsantos1@gmail.com>
2018-01-12 04:37:34 -0200

    add Pausable

--- Assets/2Dverse/Sandbox/Pausable.cs
+++ Assets/2Dverse/Sandbox/Pausable.cs
@@ -1,7 +1,26 @@
-﻿namespace DefaultNamespace {
-	public class Pausable {
+﻿using UnityEngine;
 
-		
+public abstract class Pausable : MonoBehaviour {
 
+	private bool paused;
+
+	protected void Start () {
+		PauseButton.Instance.OnPause += HandlePause;
 	}
+
+	private void Update () {
+		if (paused) return;
+		PausableUpdate();
+	}
+
+	private void HandlePause (bool pause) {
+		paused = pause;
+	}
+
+	protected abstract void PausableUpdate ();
+
+	private void OnDestroy () {
+		PauseButton.Instance.OnPause -= HandlePause;
+	}
+
 }
---
+++ Assets/2Dverse/Sandbox/PauseButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour {
+
+	private Button button;
+
+	private bool paused;
+	
+	public static PauseButton Instance { get; private set; }
+
+	public delegate void OnPauseSwitch (bool paused);
+	public event OnPauseSwitch OnPause;
+
+	private void Awake () {
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Button>().onClick.AddListener(Switch);
+		SceneLoader.Instance.OnSceneLoad += ReturnTimeScale;
+	}
+
+	private void ReturnTimeScale () {
+		Time.timeScale = 1;
+	}
+
+	private void Switch () {
+		paused = !paused;
+		Time.timeScale = paused ? 0 : 1;
+		OnPause?.Invoke(paused);
+	}
+
+	private void OnDestroy () {
+		SceneLoader.Instance.OnSceneLoad -= ReturnTimeScale;
+	}
+
+}
