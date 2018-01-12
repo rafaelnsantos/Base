d8164c9b551c6cdf8219f641cee0f855f3b42aa7
Rafael Santos <rafaelnsantos1@gmail.com>
2018-01-12 13:07:04 -0200

    testes

---
+++ Assets/_Projeto/Sandbox/InfoButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoButton : MonoBehaviour {
+
+
+	public GameObject InfoPanel;
+
+	private void Start () {
+		GetComponent<Button>().onClick.AddListener(Show);
+	}
+
+	public void Show () {
+		InfoPanel.SetActive(true);
+	}
+
+	public void Hide () {
+		InfoPanel.gameObject.GetComponentInChildren<Animator>().SetBool("Hide", true);
+	}
+
+}
---
+++ Assets/_Projeto/Sandbox/MyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : Pausable {
+
+	private new void Start () {
+		base.Start();
+		Debug.Log("asd");
+		// ...
+	}
+
+	protected override void PausableUpdate () {
+		Debug.Log("Running");
+	}
+
+	private new void OnDestroy () {
+		base.OnDestroy();
+		// ...
+	}
+
+}
