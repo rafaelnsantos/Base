c5a4081124f9983f30c0c3d68aba6c69ecb28657
Rafael Santos <rafaelnsantos1@gmail.com>
2018-03-23 03:18:47 -0300

    ios first

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,15 +2,6 @@
 
 project="<YOUR PROJECT NAME HERE>"
 
-echo "Attempting to build $project for Android"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildTarget "Android"
-  -quit
-
 echo "Attempting to build $project for iOS"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity 
   -batchmode 
@@ -19,6 +10,16 @@ echo "Attempting to build $project for iOS"
   -logFile $(pwd)/unity.log 
   -projectPath $(pwd) 
   -buildTarget "iOS"
+  -quit
+
+echo "Attempting to build $project for Android"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildTarget "Android"
   -quit
 
 echo 'Logs from build'
