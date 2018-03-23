c305b2ea4249fc8e9c9b842e98d7a30b692e71b8
Rafael Santos <rafaelnsantos1@gmail.com>
2018-03-23 03:06:28 -0300

    crasehs

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -6,7 +6,6 @@ echo "Attempting to build $project for Android"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity 
   -batchmode 
   -nographics 
-  -silent-crashes 
   -logFile $(pwd)/unity.log 
   -projectPath $(pwd) 
   -buildTarget "Android"
